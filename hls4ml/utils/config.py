e2bc338b381ed7d880c7bf07dfa65063819bdb43
Rafael Teixeira de Lima <rafael.teixeira.de.lima@cern.ch>
2021-07-19 10:44:29 +0200

    Update config.py

--- hls4ml/utils/config.py
+++ hls4ml/utils/config.py
@@ -102,10 +102,11 @@ def config_from_keras_model(model, granularity='model', default_precision='ap_fi
     activation_layers = ['Activation', 'LeakyReLU', 'ThresholdedReLU', 'ELU', 'PReLU', 'Softmax', 'ReLU']
     merge_layers = ['Add', 'Subtract', 'Multiply', 'Average', 'Maximum', 'Minimum', 'Concatenate', 'Dot']
     qkeras_layers = ['QDense', 'QActivation', 'QConv1D', 'QConv2D', 'QBatchNormalization', 'QConv2DBatchnorm']
+    recurrent_layers = ['LSTM', 'GRU']
     #Define layers to skip for conversion to HLS
     skip_layers = ['Dropout', 'Flatten']
     #All supported layers
-    supported_layers = core_layers + dense_layers + conv_layers + pooling_layers + norm_layers + activation_layers + merge_layers + qkeras_layers + skip_layers
+    supported_layers = core_layers + dense_layers + conv_layers + pooling_layers + norm_layers + activation_layers + merge_layers + qkeras_layers + skip_layers + recurrent_layers
 
     keras_layer_config = None
     if model_arch['class_name'] == 'Sequential':
